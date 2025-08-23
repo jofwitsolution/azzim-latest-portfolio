335e63ceb41cde5a313ff285ffc2f59c92cbea6f
Faleye Oluwafemi Joseph <faleyeoluwafemi1@gmail.com>
2025-08-23 22:57:26 +0100

    change params

--- app/api/blogs/[slug]/route.ts
+++ app/api/blogs/[slug]/route.ts
@@ -1,13 +1,13 @@
-import { NextRequest } from "next/server";
-import { dbConnect } from "@/lib/db/mongoose";
+import {NextRequest} from "next/server";
+import {dbConnect} from "@/lib/db/mongoose";
 import BlogModel from "@/models/Blog";
 
-export async function GET(_req: NextRequest, { params }: { params: { slug: string } }) {
-  await dbConnect();
-  const { slug } = params;
-  const doc = await BlogModel.findOne({ slug }).lean();
-  if (!doc) {
-    return new Response(JSON.stringify({ message: "Not found" }), { status: 404 });
-  }
-  return Response.json(doc);
+export async function GET(_req: NextRequest, {params}: { params: Promise<{ slug: string }> }) {
+    await dbConnect();
+    const {slug} = await params;
+    const doc = await BlogModel.findOne({slug}).lean();
+    if (!doc) {
+        return new Response(JSON.stringify({message: "Not found"}), {status: 404});
+    }
+    return Response.json(doc);
 }
